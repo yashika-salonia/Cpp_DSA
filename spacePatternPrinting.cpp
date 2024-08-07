e7a9ec38b45897684b0399c9c8c01e3bbe8dce98
yashika-salonia <yashikasalonia20@gmail.com>
2024-08-07 22:56:11 +0530

    adding files

--- spacePatternPrinting.cpp
+++ spacePatternPrinting.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(){
     /* pattern-1
-           *
-          **
-         ***
-        ****
+            *
+          * *
+        * * *
+      * * * *
+    * * * * *
     */
     int i=1,j,n;
     cout<<"enter n: ";
@@ -13,21 +14,21 @@ int main(){
     while(i<=n){
         j=n-1;
         while(j>=i){
-            cout<<" ";
+            cout<<" "<<" ";
             j--;
         }
         j=1;
         while(j<=i){
-            cout<<"*";
+            cout<<"*"<<" ";
             j++;
         }
         cout<<"\n";
         i++;
     }
     /* pattern-2
-        ****
-        ***
-        **
+        * * * *
+        * * *
+        * *
         *
     */
     int n1;
@@ -37,17 +38,17 @@ int main(){
     while(i<=n){
         j=n1;
         while(j>=i){
-            cout<<"*";
+            cout<<"*"<<" ";
             j--;
         }
         cout<<"\n";
         i++;
     }
     /* pattern-3
-        ****
-         ***
-          **
-           *
+        * * * *
+          * * *
+            * *
+              *
     */
     int n2;
     cout<<"enter n: ";
@@ -56,22 +57,22 @@ int main(){
     while(i<=n2){
         j=1;
         while(j<i){
-            cout<<" ";
+            cout<<" "<<" ";
             j++;
         }
         j=n2;
         while(j>=i){
-            cout<<"*";
+            cout<<"*"<<" ";
             j--;
         }
         cout<<"\n";
         i++;
     }
     /* pattern-4
-        1111
-         222
-          33
-           4
+        1 1 1 1
+          2 2 2
+            3 3
+              4
     */
     int n3;
     cout<<"enter n: ";
@@ -81,12 +82,12 @@ int main(){
     while(i<=n3){
         j=1;
         while(j<i){
-            cout<<" ";
+            cout<<" "<<" ";
             j++;
         }
         j=n3;
         while(j>=i){
-            cout<<count;
+            cout<<count<<" ";
             j--;
         }
         count++;
@@ -94,10 +95,10 @@ int main(){
         i++;
     }
     /* pattern-4
-           1
-          22
-         333
-        4444
+              1
+            2 2
+          3 3 3
+        4 4 4 4
     */
     int n4;
     cout<<"enter n: ";
@@ -107,12 +108,12 @@ int main(){
     while(i<=n4){
         j=n4-1;
         while(j>=i){
-            cout<<" ";
+            cout<<" "<<" ";
             j--;
         }
         j=1;
         while(j<=i){
-            cout<<count;
+            cout<<count<<" ";
             j++;
         }
         count++;
@@ -120,10 +121,10 @@ int main(){
         i++;
     }
     /* pattern-5
-        1234
-         234
-          34
-           4
+        1 2 3 4
+          2 3 4
+            3 4
+              4
     */
     int n5;
     cout<<"enter n: ";
@@ -133,13 +134,13 @@ int main(){
     while(i<=n5){
         j=1;
         while(j<i){
-            cout<<" ";
+            cout<<" "<<" ";
             j++;
         }
         j=n5;
         count=i;
         while(j>=i){
-            cout<<count;
+            cout<<count<<" ";
             count++;
             j--;
         }
@@ -149,10 +150,10 @@ int main(){
         i++;
     }
     /* pattern-6
-           1
-          23
-         456
-        78910
+              1
+            2 3
+          4 5 6
+        7 8 9 10
     */
     int n6;
     cout<<"enter n: ";
@@ -162,16 +163,95 @@ int main(){
     while(i<=n6){
         j=n6-1;
         while(j>=i){
-            cout<<" ";
+            cout<<" "<<" ";
             j--;
         }
         j=1;
         while(j<=i){
-            cout<<count;
+            cout<<count<<" ";
             count++;
             j++;
         }
         cout<<"\n";
         i++;
+    } 
+    /* pattern-7
+              1
+            1 2 1
+          1 2 3 2 1
+        1 2 3 4 3 2 1
+    */
+    int n7;
+    cout<<"enter n: ";
+    cin>>n7;
+    i=1;
+    count=1;
+    while(i<=n7){
+        //printing space
+        j=n7-1;
+        while(j>=i){
+            cout<<" "<<" ";
+            j--;
+        }
+        //printing 1st triangle
+        j=1;
+        while(j<=i){
+            cout<<j<<" ";
+            j++;
+        }
+        //printing 2nd triangle
+        j=1;
+        count=i-1;
+        while(j<=i-1){
+            cout<<count<<" ";
+            count--;
+            j++;
+        }
+        cout<<endl;
+        count++;
+        i++;
+    }
+    
+    /* pattern-7
+        1 2 3 4 5 5 4 3 2 1
+        1 2 3 4 * * 4 3 2 1
+        1 2 3 * * * * 3 2 1
+        1 2 * * * * * * 2 1
+        1 * * * * * * * * 1
+    */
+    int n8;
+    cout<<"enter n: ";
+    cin>>n8;
+    i=1;
+    while(i<=n8){
+        j=n8;
+        count=1;
+        while(j>=i){
+            cout<<count<<" ";
+            count++;
+            j--;
+        }
+
+        j=1;
+        while(j<=i-1){
+            cout<<"*"<<" ";
+            j++;
+        }
+
+        j=1;
+        while(j<=i-1){
+            cout<<"*"<<" ";
+            j++;
+        }
+
+        j=n8;
+        count=n8+1-i;
+        while(j>=i){
+            cout<<count<<" ";
+            count--;
+            j--;
+        }
+    cout<<endl;
+    i++;
     }
 }
